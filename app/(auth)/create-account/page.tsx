ff4fa801396b532aed10fa9b70552cf4008d2fe0
LeeYunHak <sza0203@naver.com>
2025-05-07 21:37:29 +0900

    style(CreateAccountPage): 디자인 컨셉 반영 [#13]

--- app/(auth)/create-account/page.tsx
+++ app/(auth)/create-account/page.tsx
@@ -2,7 +2,7 @@
 import FormButton from '@/components/ui/form/FormButton';
 import FormInput from '@/components/ui/form/FormInput';
 import { ACCOUNT_VALIDATION } from '@/lib/constants';
-import { Smile } from 'lucide-react';
+import { FileText } from 'lucide-react';
 import { startTransition, useActionState } from 'react';
 import { createAccount } from './actions';
 
@@ -16,11 +16,11 @@ export default function CreateAccountPage() {
   };
 
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <section className="w-full max-w-md rounded-lg p-8">
-        <figure className="mb-8 flex flex-col items-center justify-center gap-2 *:font-medium">
-          <Smile className="size-14 text-cyan-400" />
-          <h2 className="text-xl">Fill in the form below to join!</h2>
+    <main className="flex min-h-screen items-center justify-center px-4">
+      <section className="retro-container w-full max-w-md">
+        <figure className="mb-8 flex flex-col items-center justify-center gap-2">
+          <FileText className="size-14 text-blue-600" />
+          <h2 className="text-xl text-blue-600">Create Account</h2>
         </figure>
         <form onSubmit={onSubmit} className="space-y-4">
           <FormInput
@@ -58,7 +58,7 @@ export default function CreateAccountPage() {
             minLength={ACCOUNT_VALIDATION.MIN_LENGTH.PASSWORD}
             required
           />
-          <FormButton text="Create account" type="submit" />
+          <FormButton text="계정 만들기" type="submit" />
         </form>
       </section>
     </main>
